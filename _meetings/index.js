027b17e909cd0624c54509b9bcd29fcfa109dbd1
Paul Ardeleanu <pardel@gmail.com>
2022-10-09 22:29:20 +0100

    Added webhooks for meetings

--- _meetings/index.js
+++ _meetings/index.js
@@ -1,21 +1,13 @@
+let { showRequest } = require('../utils/utils.js');
+
 module.exports.meetingsSetup = (app, vonage) => {
 
-  
   app.get('/meetings', (request, response) => {
-    const meetingStatus = request.cookies.meetingStatus;
-    response.clearCookie('meetingStatus');
-    const meetingData = request.cookies.meetingData;
-    response.clearCookie('meetingData');
-
     const jwt = vonage.generateJwt();
-
-    response.render('_meetings/index', { meetingStatus, meetingData, jwt });
+    response.render('_meetings/index', { jwt });
   });
 
-
-  app.post('/meetings/create', (request, response) => {
-    response.redirect('/meetings');
-  });
-
+  app.post('/meetings/rooms', showRequest);
+  app.post('/meetings/sessions', showRequest);
 
 };
